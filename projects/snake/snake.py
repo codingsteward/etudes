c0ac21789be1ed580c7061149ec3d1f57e6f0658
codingsteward <codingsteward@gmail.com>
2025-02-25 02:35:18 +0800

    add snake

---
+++ projects/snake/snake.py
@@ -0,0 +1,39 @@
+
+
+
+
+class Snake:
+
+    def __init__(self, rows=128, cols=128):
+        self.snake_head = None
+        self.snake_body = None
+        self.food = None
+        self.ROWS = rows
+        self.COLS = cols
+
+    def move(direction) -> None:
+        # update head to new location
+        next_coord = self.get_next_coord(direction)
+        if is_collision(next_coord):
+            return
+        if is_eat_food():
+            self.points += 1
+            self.spawn_new_food()
+        else:
+            self.snake_body.popleft()
+
+    def spawn_new_food() -> None:
+        pass
+
+    def is_collision(next_coord) -> bool:
+        if (next_coord.x < 0 or next_coord.x >= self.COLS or
+            next_coord.y < 0 or next_coord.y >= self.ROWS):
+            return True
+        if next_coord in self.snake_body:
+            return True
+
+        return False 
+    def is_eat_food() -> bool:
+        pass
+
+
